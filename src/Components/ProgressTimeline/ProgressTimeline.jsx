023ab9bb2f5b24fb63ecc166d470387b98be0581
Adham Nabil <adhamnabil440@gmail.com>
2025-01-21 23:44:50 +0200

    fixed timeline issues (#1)

--- src/Components/ProgressTimeline/ProgressTimeline.jsx
+++ src/Components/ProgressTimeline/ProgressTimeline.jsx
@@ -2,13 +2,46 @@ import TimelineDisplay from "../TimelineDisplay/TimelineDisplay";
 import { useEffect, useState } from "react";
 
 export default function ProgressTimeline({ order }) {
-  const [orderedEvents, setOrderedEvents] = useState([]);
+  const [eventsGroupedByDate, setEventsGroupedByDate] = useState({});
+  const [orderedDates, setOrderedDates] = useState([]);
 
-  //show latest events first on timeline
+  // Grouping the transit events by date
   useEffect(() => {
     if (!order.TransitEvents) return;
-    setOrderedEvents(order.TransitEvents.reverse());
-  }, [order]);
+
+    const groupedByDate = order.TransitEvents?.reduce((acc, event) => {
+      const date = new Date(event.timestamp).toISOString().split("T")[0]; // Extract the date part
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push(event);
+      return acc;
+    }, {});
+
+    // Reverse events to display newest events within each date first
+    Object.keys(groupedByDate).forEach((date) => {
+      groupedByDate[date].reverse();
+    });
+
+    // Sort dates in a new array to show latest first
+    const reversedDates = Object.keys(groupedByDate).sort(
+      (a, b) => new Date(b) - new Date(a)
+    );
+    setOrderedDates(reversedDates);
+    setEventsGroupedByDate(groupedByDate);
+  }, []);
+
+  //function to format the date
+  function dateFormat(date) {
+    if (!date) return;
+
+    const formattedDate = new Date(date).toLocaleDateString(undefined, {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+    });
+    return formattedDate;
+  }
 
   return (
     <div className="max-w-[900px] mx-auto px-4">
@@ -24,9 +57,24 @@ export default function ProgressTimeline({ order }) {
 
           {/* timeline */}
           <div className="my-6">
-            {orderedEvents?.map((event, index) => (
-              <TimelineDisplay event={event} key={index} />
-            ))}
+            {orderedDates.map((date) => {
+              return (
+                <div
+                  key={date}
+                  className="relative after:absolute after:top-[25px] after:left-[7px] after:w-[2px] after:h-[100%] after:bg-[#d0d5dd] after:content-['']"
+                >
+                  <div className="flex items-center justify-start gap-2 my-6">
+                    <div className="w-[15px] h-[15px] bg-[#d0d5dd] rounded-full"></div>
+                    <h2 className="text-[black] font-[600]">
+                      {dateFormat(date)}
+                    </h2>
+                  </div>
+                  {eventsGroupedByDate[date]?.map((event, index) => (
+                    <TimelineDisplay event={event} key={index} />
+                  ))}
+                </div>
+              );
+            })}
           </div>
         </>
       )}
--- src/Components/TimelineDisplay/TimelineDisplay.jsx
+++ src/Components/TimelineDisplay/TimelineDisplay.jsx
@@ -1,30 +1,13 @@
 import { useEffect, useState } from "react";
 
 export default function TimelineDisplay({ event }) {
-  const [eventDate, setEventDate] = useState(null);
   const [eventTime, setEventTime] = useState(null);
 
-  // get date and time in required format
+  // get time in required format
   useEffect(() => {
-    const date = dateFormat(event.timestamp);
     const time = timeFormat(event.timestamp);
-    setEventDate(date);
     setEventTime(time);
   }, [event]);
-
-  //function to format the date
-  function dateFormat(timestamp) {
-    if (!timestamp) return;
-
-    const date = new Date(timestamp);
-    const formattedDate = new Intl.DateTimeFormat("en-US", {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    }).format(date);
-    return formattedDate;
-  }
 
   //function to format the time
   function timeFormat(timestamp) {
@@ -32,8 +15,8 @@ export default function TimelineDisplay({ event }) {
 
     const date = new Date(timestamp);
 
-    let hours = date.getUTCHours();
-    const minutes = date.getUTCMinutes();
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
     const period = hours >= 12 ? "PM" : "AM";
 
     hours = hours % 12 || 12;
@@ -44,17 +27,9 @@ export default function TimelineDisplay({ event }) {
   }
 
   return (
-    <div>
-      <div className="relative my-2 after:absolute after:top-[23%] after:left-[6px] after:w-[3px] after:h-[80%] after:bg-[#d0d5dd] after:content-['']">
-        <div className="flex items-center justify-start gap-2 mb-2">
-          <div className="w-[15px] h-[15px] bg-[#d0d5dd] rounded-full"></div>
-          <h2 className="text-[black] font-[600]">{eventDate}</h2>
-        </div>
-        <div className="ml-6 border border-[#d0d5dd] px-4 py-2 rounded-[5px] font-[400]">
-          {event.state}
-          <p className="text-[#667085]">{eventTime}</p>
-        </div>
-      </div>
+    <div className="ml-6 border border-[#d0d5dd] px-4 py-2 my-2 rounded-[5px] font-[400]">
+      {event.state}
+      <p className="text-[#667085]">{eventTime}</p>
     </div>
   );
 }
